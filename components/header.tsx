10ac37fd04da441d35c5a13e5a8c7c05addfc0fc
MedELHattab <136116042+MedELHattab@users.noreply.github.com>
2025-12-23 00:08:07 +0100

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react"
 import Link from "next/link"
 import Image from "next/image"
-import { Menu, X, Phone, Mail } from "lucide-react"
+import { Menu, X} from "lucide-react"
 
 export function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
